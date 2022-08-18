96eed8efdffbb6c32de30d4e3a30e82553dc2dd8
Ocksana Bondarenko <bondarrren777@gmail.com>
2022-08-18 14:10:11 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,8 @@
-
 const categoriesEl = document.querySelectorAll('.item');
 console.log('Number of categories:', categoriesEl.length);
 
-categoriesEl.forEach(function (categories) {
+categoriesEl.forEach (function (categories) {
           console.log ('Category:', categories.firstElenentChild.textContent);
           console.log ('Elements:', categories.lastElenentChild.children.length);
-})
+          }
+)
